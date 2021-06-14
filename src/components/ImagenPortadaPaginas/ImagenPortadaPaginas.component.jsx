e3e7b973a5bc530119fb4326eb85c3f66e75232f
JoseLuis <luiscoche9@gmail.com>
2021-06-14 08:37:31 -0500

    Voy por la torta

--- src/components/ImagenPortadaPaginas/ImagenPortadaPaginas.component.jsx
+++ src/components/ImagenPortadaPaginas/ImagenPortadaPaginas.component.jsx
@@ -2,7 +2,17 @@ import React, { useContext } from 'react';
 import imagenPortada from '../../imgs/bread.jpg';
 import imagenDesayunos from '../../imgs/desayunoCuadrado.jpg';
 import ThemeContext from '../../providers/Theme/Theme.provider';
-import { ImagenPortadaStyled, DetalleAlmuerzos, DescripcionDesayuno, ContenedorImagen, TituloDesayuno, ImagenDesayuno } from './ImagenPortadaPaginas.styling';
+import { ImagenPortadaStyled, TablaDesayunos, DetalleAlmuerzos, ContainerDesayunosLista, DescripcionDesayunoPrecio, CuadroBlanco, DescripcionDesayuno, ContenedorImagen, TituloDesayuno, ImagenDesayuno } from './ImagenPortadaPaginas.styling';
+
+
+const desayunosAlmuerzosLabel = [
+    { title: 'Cuatro hotcakes con maple', precio: '$45' },
+    { title: 'Cuatro hotcakes con fruta', precio: '$60' },
+    { title: <span><span style={{color:'purple'}}>Básico</span> (2 huevos, 2 hotcakes y tocino)</span>, precio: '$50' },
+    { title: <span><span style={{color:'green'}}>Americano</span> (2 huevos, tocino, papa rallada)</span>, precio: '$65' },
+    { title: <span><span style={{color:'red'}}>Completo</span> (2 huevos, frijoles, pan, una proteína (arrachera, prensado o chorizo))</span>, precio: '$70' },
+    { title: 'Hot cake por piezas', precio: '$17' },
+]
 
 const ImagenPortadaPaginas = () => {
 
@@ -13,7 +23,7 @@ const ImagenPortadaPaginas = () => {
             <ImagenPortadaStyled img={imagenPortada}>
 
             </ImagenPortadaStyled>
-            <div style={{textAlign:'center'}}>
+            <div style={{ textAlign: 'center' }}>
                 <h1 className="title">¿Ya tienes que desayunar?</h1>
             </div>
             <DetalleAlmuerzos color={cremaColor}>
@@ -21,11 +31,36 @@ const ImagenPortadaPaginas = () => {
                     <DescripcionDesayuno>
                         <TituloDesayuno>Desayunos y almuerzos</TituloDesayuno>
                         <p className="fontFSpecialElite">DISPONIBLE DESDE LAS 9 AM</p>
+                        <CuadroBlanco>
+                            <div style={{
+                                paddingLeft: '20px',
+                                width: '70%',
+                                height: '100%'
+                            }}>
+                                <p className="fontFSpecialElite" style={{ textAlign: 'left' }}>Desayunos</p>
+                                <ContainerDesayunosLista style={{ textAlign: 'left', marginLeft: '40px', position: 'absolute', top: '60px', bottom: '' }}>
+                                    {
+                                        desayunosAlmuerzosLabel.map(e => {
+                                            return (
+                                                <TablaDesayunos>
+                                                    <DescripcionDesayunoPrecio className="fontFSpecialElite">{e.title}</DescripcionDesayunoPrecio>
+                                                    <DescripcionDesayunoPrecio className="fontFSpecialElite">{e.precio}</DescripcionDesayunoPrecio>
+                                                </TablaDesayunos>
+                                            )
+                                        })
+                                    }
+                                    {/* <TablaDesayunos>
+                                        <p className="fontFSpecialElite" style={{textAlign: 'left', display: 'inline-block'}}>Cuatro hotcakes con maple</p>  
+                                        <p className="fontFSpecialElite" style={{textAlign: 'left', display: 'inline-block'}}>$45</p>  
+                                    </TablaDesayunos> */}
+                                </ContainerDesayunosLista>
+                            </div>
+                        </CuadroBlanco>
                     </DescripcionDesayuno>
                     <ContenedorImagen>
-                        <ImagenDesayuno img={imagenDesayunos}/>
+                        <ImagenDesayuno img={imagenDesayunos} />
                     </ContenedorImagen>
-                    
+
                 </div>
             </DetalleAlmuerzos>
             <div style={{ height: '300px' }}>a</div>
--- src/components/ImagenPortadaPaginas/ImagenPortadaPaginas.styling.jsx
+++ src/components/ImagenPortadaPaginas/ImagenPortadaPaginas.styling.jsx
@@ -20,23 +20,63 @@ export const DetalleAlmuerzos = styled.div`
 export const DescripcionDesayuno = styled.div`
     width: 50%;
     text-align: center;
+    overflow: hidden;
+    position: relative;
 `;
 
 export const TituloDesayuno = styled.h1`
     text-align: center;
+    font-size: 30pt;
 `;
 
 export const ContenedorImagen = styled.div`
     width: 50%;
-    height: 95%;
+    position: relative;
 `;
 
 export const ImagenDesayuno = styled.div`
     background-image: url(${props => props.img});
-    background-position: top;
+    background-position: center;
     background-size: contain;
     background-repeat: no-repeat;
     width: 100%;
-    height: 100%;
-    margin-top: -30px;
+    height: 94%;
+    /* margin-top: -30px; */
+    position: absolute;
+    bottom: 40px;
+`;
+
+export const CuadroBlanco = styled.div`
+    background-color: white;
+    margin-left: 40px;
+    padding: 20px;
+    position: absolute;
+    bottom: 40px;
+    top: 130px;
+    width: 100%;
+`;
+
+export const ContainerDesayunosLista = styled.div`
+    text-align: left;
+    margin-left: 40px;
+    position: absolute;
+    width: 70%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const TablaDesayunos = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    margin: 0px;
+    width: 100%;
+    flex: 1;
+`;
+
+export const DescripcionDesayunoPrecio = styled.p`
+    text-align: left;
+    display: block;
+    margin: 0px;
 `;
