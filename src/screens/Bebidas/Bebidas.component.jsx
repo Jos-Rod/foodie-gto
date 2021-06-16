89a7255cd93d3c0d58283d6b4eb237307e208506
JoseLuis <luiscoche9@gmail.com>
2021-06-16 08:30:40 -0500

    bebidas puestas

--- src/screens/Bebidas/Bebidas.component.jsx
+++ src/screens/Bebidas/Bebidas.component.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 import ImagenPortadaPaginas from '../../components/ImagenPortadaPaginas/ImagenPortadaPaginas.component';
 import imagenPortada from '../../imgs/bebidasPortada.jpg';
-import {ContenedorGeneralBebidas, ContenedorNegro, ContenedorInformacion} from './Bebidas.styling';
+import imagenOrangeEnergizer from '../../imgs/bebidas/orangeEnergizer.jpg';
+import imagenMojitoMango from '../../imgs/bebidas/mojitoMango.jpg';
+import imagenMojitoFresa from '../../imgs/bebidas/mojitoFresa.jpg';
+import imagenLinternaVerde from '../../imgs/bebidas/linternaVerde.jpg';
+import imagenSmoothieGansito from '../../imgs/bebidas/smoothieGansito.jpg';
+import imagenSmoothieFresa from '../../imgs/bebidas/smoothieFresa.jpg';
+import imagenSmoothieChicle from '../../imgs/bebidas/smoothieChicle.jpg';
+import imagenSmoothieTaro from '../../imgs/bebidas/smoothieTaro.jpg';
+import { ContenedorGeneralBebidas, ImagenBebidaStyle, ContenedorBebidaStyle, ContenedorNegro, ContenedorInformacion } from './Bebidas.styling';
+
+const listaBebidas = [
+    { title: 'Orange Energizer', img: imagenOrangeEnergizer },
+    { title: 'Mojito de mango', img: imagenMojitoMango },
+    { title: 'Mojito de fresa', img: imagenMojitoFresa },
+    { title: 'Linterna verde', img: imagenLinternaVerde },
+    { title: 'Smoothie de gansito', img: imagenSmoothieGansito },
+    { title: 'Smoothie de fresa', img: imagenSmoothieFresa },
+    { title: 'Smoothie de chicle', img: imagenSmoothieChicle },
+    { title: 'Smoothie de taro', img: imagenSmoothieTaro },
+];
+
+const ContenedorBebida = ({ title, img }) => {
+    return (
+        <>
+            <ContenedorBebidaStyle>
+                <ImagenBebidaStyle title={title} img={img} />
+                <p style={{ marginTop: '10px' }}>{title}</p>
+            </ContenedorBebidaStyle>
+        </>
+    )
+}
 
 const Bebidas = () => {
     return (
@@ -11,9 +41,18 @@ const Bebidas = () => {
                 <ContenedorNegro />
                 <ContenedorInformacion>
                     <h1 className="title">Disfrútalas bien frías por sólo $45</h1>
+                    <div style={{ display: 'contents', height: '100%' }}>
+                        {
+                            listaBebidas.map(e => {
+                                return (
+                                    <ContenedorBebida title={e.title} img={e.img} />
+                                )
+                            })
+                        }
+                    </div>
                 </ContenedorInformacion>
             </ContenedorGeneralBebidas>
-            <div style={{height: '10vh'}}></div>
+            <div style={{ height: '10vh' }}></div>
         </>
     )
 }
--- src/screens/Bebidas/Bebidas.styling.jsx
+++ src/screens/Bebidas/Bebidas.styling.jsx
@@ -2,15 +2,19 @@ import styled from 'styled-components';
 
 export const ContenedorGeneralBebidas = styled.div`
     height: 90vh;
-    background-color: brown;
+    /* background-color: brown; */
     margin-top: 60px;
     min-height: 790px;
     position: relative;
+
+    @media screen and (min-width: 1500px) {
+        min-height: 1100px;
+    }
 `;
 
 export const ContenedorNegro = styled.div`
     background-color: black;
-    height: 40%;
+    height: 37%;
     width: 100%;
     position: absolute;
     top: 0px;
@@ -23,4 +27,25 @@ export const ContenedorInformacion = styled.div`
     top: 5%;
     color: white;
     text-align: center;
+`;
+
+export const ContenedorBebidaStyle = styled.div`
+    width: 25%;
+    height: 40%;
+    color: black;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    justify-items: center;
+`;
+
+export const ImagenBebidaStyle = styled.div`
+    background-image: url(${props => props.img});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 80%;
+    height: 70%;
 `;
